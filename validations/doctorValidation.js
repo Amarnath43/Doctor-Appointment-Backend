663c37b6ae2c87ebaf4ecd7efd87a8b66aa4831b
Amarnath43 <amarnathgoshika@gmail.com>
2025-06-20 02:30:13 +0530

    updated doctor validation-2

--- validations/doctorValidation.js
+++ validations/doctorValidation.js
@@ -1,16 +1,17 @@
 const Joi=require('joi');
 
-const availabilitySchema=Joi.object({
+/*const availabilitySchema=Joi.object({
             date: Joi.date().iso().required(),
             slots: Joi.array().items(
                 Joi.string()
             )
         }
         )
+        */ 
     
 
 const doctorSchema=Joi.object({
-name: Joi.string().min(3).max(20).required(),
+    name: Joi.string().min(3).max(20).required(),
     email:Joi.string().email().required(),
     phone: Joi.string().pattern(/^[6-9]{1}[0-9]{9}$/).required(),
     password: Joi.string().min(3).required(),
@@ -19,8 +20,7 @@ name: Joi.string().min(3).max(20).required(),
     hospitalName: Joi.string().min(3).max(50).required(),
     fee: Joi.number().min(100).max(10000).required(),
     bio: Joi.string().min(10).max(300).required(),
-    availability: Joi.array().items(availabilitySchema),
-    status: Joi.string().valid('active', 'pending', 'blocked')
+    //availability: Joi.array().items(availabilitySchema),
 
 });
 
